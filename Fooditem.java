feb11523cc5424c8e5d715b89ba445180a3b88fe
Mithra0083 <mithrap2604@gmail.com>
2025-11-01 21:09:55 +0530

    Fooditem

---
+++ Fooditem.java
@@ -0,0 +1,11 @@
+package Model;
+
+public class Fooditem { private String name; private double price;
+
+public FoodItem(String name, double price) { this.name = name; this.price = price; }
+
+public String getName() { return name; }
+
+public double getPrice() { return price; }
+
+@Override public String toString() { return name + "-" + price; } }
